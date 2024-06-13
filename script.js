8e78b6b93b41021bb58f0a295051abd916c0a37b
Heidi <Heidi.devilliers060@gmail.com>
2024-06-13 23:18:40 +0200

    error fix

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const jsonStr = JSON.stringify({ site: 'bobbyhadz.com' });
+JSON.parse(jsonStr); // No error
+
+$(document).ready(() => {
+  $('.login-form').on('submit', function (e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          dataType: "json", // Automatically parses the response as JSON
+          timeout: 500,
+          url: '/src/login.php',
+          data: $(this).serialize(),
+          success: (data) => {
+              console.log(data); // No need for JSON.parse
+          },
+          error: (data) => {
+              console.log(data);
+          }
+      });
+  });
+});
